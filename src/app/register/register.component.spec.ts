def5ef9b5c64984af1c4796ea45a201dac068eee
JoseVSeb <josevseb99@gmail.com>
2021-11-20 23:56:32 +0530

    remove unimplemented errors in tests

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ApiService } from '../service/api.service';
 
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let mockApiService: jasmine.SpyObj<ApiService>;
 
   beforeEach(async () => {
+    mockApiService = jasmine.createSpyObj("ApiService", ["registerStudent"]);
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      providers: [
+        { provide: ApiService, useValue: mockApiService }
+      ]
     })
     .compileComponents();
   });
--- src/app/view/view.component.spec.ts
+++ src/app/view/view.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ApiService } from '../service/api.service';
 
 import { ViewComponent } from './view.component';
 
 describe('ViewComponent', () => {
   let component: ViewComponent;
   let fixture: ComponentFixture<ViewComponent>;
+  let mockApiService: jasmine.SpyObj<ApiService>;
 
   beforeEach(async () => {
+    mockApiService = jasmine.createSpyObj("ApiService", ["listStudents"]);
     await TestBed.configureTestingModule({
-      declarations: [ ViewComponent ]
+      declarations: [ ViewComponent ],
+      providers: [
+        { provide: ApiService, useValue: mockApiService }
+      ]
     })
     .compileComponents();
   });
