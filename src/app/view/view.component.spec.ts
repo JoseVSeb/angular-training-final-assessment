fbd0fad67219be15073dd14081dd779700308c5f
JoseVSeb <josevseb99@gmail.com>
2021-11-21 11:20:56 +0530

    tests for view component

--- src/app/view/view.component.spec.ts
+++ src/app/view/view.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import * as _ from 'lodash';
+import { of } from 'rxjs';
+import { Student } from 'src/interfaces/student';
 import { ApiService } from '../service/api.service';
 
 import { ViewComponent } from './view.component';
@@ -22,10 +25,52 @@ describe('ViewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ViewComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should list all students`, () => {
+    const compiled = fixture.nativeElement as HTMLElement
+
+    const expected: Student[] = [
+      {
+        name: "test",
+        rollNo: 3,
+        email: "test@abc.com",
+        department: "abc",
+        phoneNo: 1234567890,
+      },
+      {
+        name: "qwerty",
+        rollNo: 5,
+        email: "asdf@abc.com",
+        department: "abc",
+        phoneNo: 9012345678,
+      },
+    ]
+
+    type Key = 'name'| 'rollNo' | 'email' | 'department' | 'phoneNo'
+
+    const keys = <Key[]>Object.keys(expected[0])
+
+    mockApiService.listStudents.and.returnValue(of(expected))
+
+    fixture.detectChanges()
+    
+    expect(mockApiService.listStudents).toHaveBeenCalledTimes(1)
+
+    const actual = compiled.querySelectorAll("tbody tr")
+
+    expect(actual.length).toBe(expected.length)
+
+    actual.forEach((row, index) => {
+      keys.forEach(key => {
+        const actualValue = row.querySelector(`.${_.camelCase("student " + key)}`)?.textContent
+        const expectedValue = `${expected[index][key]}`
+        expect(actualValue).toEqual(expectedValue)
+      })
+    })
+  })
 });
